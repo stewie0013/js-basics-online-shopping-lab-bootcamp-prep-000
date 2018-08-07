50e374b8f2195972be3d43aa9d5a7ad188d79426
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-07 19:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random())
+  var price = Math.floor(Math.random() * 101)
   var itemObj = {itemName: [item], itemPrice: price}
   cart.push(itemObj)
   
